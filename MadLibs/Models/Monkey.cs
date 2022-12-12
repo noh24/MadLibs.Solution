701514c135880bf469790a758d469f3ac4f6892e
Brian Noh <noh24@ymail.com>
2022-12-12 17:53:48 -0500

    add monkey class

---
+++ MadLibs/Models/Monkey.cs
@@ -0,0 +1,15 @@
+namespace MadLibs.Models
+{
+  public class Monkey
+  {
+    public string KingName {get; set;}
+    public string NatureLocation {get; set;}
+    public string Adjective1 {get; set;}
+    public string Adjective2 {get; set;}
+    public string WeaponAdjective {get; set;}
+    public string Weapon {get; set;}
+    public string Animal {get; set;}
+    public string AnimalHeroName {get; set;}
+    public string HeroWeapon {get; set;}
+  }
+}
